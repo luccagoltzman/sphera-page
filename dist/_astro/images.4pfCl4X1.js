2adacadc8ccd688eaf4e9e92f4095dd9fff6a67d
luccagoltzman <luccagoltzman@gmail.com>
2026-02-18 20:50:09 -0300

    fix: :bug: Correções para deploy

---
+++ dist/_astro/images.4pfCl4X1.js
@@ -0,0 +1,9 @@
+import{r as i}from"./index.CVf8TyFT.js";var a={exports:{}},n={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var u=i,m=Symbol.for("react.element"),d=Symbol.for("react.fragment"),l=Object.prototype.hasOwnProperty,O=u.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,x={key:!0,ref:!0,__self:!0,__source:!0};function _(t,r,f){var e,s={},p=null,c=null;f!==void 0&&(p=""+f),r.key!==void 0&&(p=""+r.key),r.ref!==void 0&&(c=r.ref);for(e in r)l.call(r,e)&&!x.hasOwnProperty(e)&&(s[e]=r[e]);if(t&&t.defaultProps)for(e in r=t.defaultProps,r)s[e]===void 0&&(s[e]=r[e]);return{$$typeof:m,type:t,key:p,ref:c,props:s,_owner:O.current}}n.Fragment=d;n.jsx=_;n.jsxs=_;a.exports=n;var E=a.exports;const o=(t,r=800)=>`https://images.unsplash.com/photo-${t}?auto=format&fit=crop&w=${r}&q=85`,R=o("1601584115197-04ecc0da31d7",1920),v={servicos:o("1621902930995-2b570d534d88"),metricas:o("1551288049-c64f5fcf8293"),depoimentos:o("1573496359142-b8d87734a5a2")},S=o("1522071820081-009f0129c71c",1920);export{S as C,R as H,v as S,E as j};
