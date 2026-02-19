18a0c4930ff04d654f0454f8798dd00f1521294b
luccagoltzman <luccagoltzman@gmail.com>
2026-02-18 23:16:56 -0300

    feat: :art: Página "Em breve"

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+/**
+ * Configuração geral do site.
+ *
+ * COMING_SOON: quando true, apenas a página "Em breve" é exibida.
+ * Mude para false para mostrar o site completo.
+ */
+export const COMING_SOON = true;
